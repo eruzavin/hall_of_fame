16726aa8da14845efbe0432b844c53fe6ef68aae
Evgenii Ruzavin <eruzavin@plesk.com>
2023-04-26 18:54:41 +0700

    Creation of the view programmatically and rendering it. Filtering by tag

--- src/Controller/hallOfFameController.php
+++ src/Controller/hallOfFameController.php
@@ -3,13 +3,29 @@
 namespace Drupal\hall_of_fame\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Drupal\Core\Render\Renderer;
 
 /**
  * Provides route responses for the Example module.
  */
 class hallOfFameController extends ControllerBase
 {
+    /**
+     * @var Renderer
+     */
+    protected $renderer;
 
+    public function __construct(Renderer $renderer) {
+        $this->renderer = $renderer;
+    }
+
+    public static function create(ContainerInterface $container) {
+        return new static(
+            $container->get('renderer')
+        );
+    }
+    
     /**
      * Returns a simple page.
      *
@@ -18,9 +34,10 @@ class hallOfFameController extends ControllerBase
      */
     public function hallOfFameView()
     {
-        return [
-            '#markup' => 'доска почета',
-        ];
-    }
+        $content = [];
+        $view = views_embed_view('awarded', 'page_1');
+        $content['#markup'] = $this->renderer->render($view);
 
+        return $view;
+    }
 }
